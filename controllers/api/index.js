069f81f7021b496cd25024d769105891e85c3c3b
lacey gibler <maxsonlacey@gmail.com>
2022-07-23 10:41:55 -0400

    env

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-const apiRouter = require('./api/');
-const homeroutes = require('./home-routes.');
+const apiRouter = require('./api');
+const homeroutes = require('./home-routes.js');
 const commentRoutes = require('./dashboard-routes');
 
 router.use('/', homeroutes);
-router.
+router. use('/api',apiRouter);
 
 module.exports = router;
